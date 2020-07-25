709463648824b0b3a26d3a9ee995daf1a7cfbbfd
azychen <antonzychen+github@gmail.com>
2020-07-25 15:08:42 -0600

    Fixed bug which prevented cube1 from resetting in setup() in testCube.py

--- src/tests/testGenerateData.py
+++ src/tests/testGenerateData.py
@@ -7,6 +7,41 @@ from src.model import scrambler, generateData
 
 # run test: py.test src/tests/testGenerateData.py
 
+
 class TestGenerateData:
 
-    singleScramble, singleStickers = scrambler.getRandomScrambles(5) 
+    singleScramble = None
+    multipleScramble = None
+    singleStickers = None
+    multipleStickers = None
+
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        singleScramble, singleStickers = scrambler.getRandomScrambles(1)
+        multipleScramble, multipleStickers = scrambler.getRandomScrambles(5)
+
+
+    def testPadScramblesSingle(self):
+        pass
+
+
+    def testPadScramblesMultiple(self):
+        pass
+
+
+    def testToSparseSingle(self):
+        pass
+
+
+    def testToSparseMultiple(self):
+        pass
+
+
+    def testFlattenStickersSingle(self):
+        pass
+
+
+    def testFlattenStickersMultiple(self):
+        pass
+
