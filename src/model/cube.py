4eb0d0950a5a19e230e336fd0801727b94a0a92b
azychen <antonzychen+github@gmail.com>
2020-07-24 18:23:13 -0600

    Renamed sides variable to stickers

--- src/model/cube.py
+++ src/model/cube.py
@@ -5,19 +5,19 @@ import copy
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-stickers = ["white", "yellow", "green", "blue", "red", "orange"]
-cmap = colors.ListedColormap(stickers + ["black"])
+stickerColors = ["white", "yellow", "green", "blue", "red", "orange"]
+cmap = colors.ListedColormap(stickerColors + ["black"])
 # moves = [rotateD, rotateDprime, rotateF, rotateFprime, rotateU, rotateUprime, rotateB, rotateBprime, rotateL, rotateLprime, rotateR, rotateRprime]
 
 
 class Cube:
     sideLen = 3
-    sides = np.empty([6, sideLen, sideLen])
+    stickers = np.empty([6, sideLen, sideLen])
 
     # constructor
     def __init__(self):
-        for i, _ in enumerate(stickers):
-            self.sides[i, :, :] = np.full([self.sideLen, self.sideLen], i)
+        for i, _ in enumerate(stickerColors):
+            self.stickers[i, :, :] = np.full([self.sideLen, self.sideLen], i)
 
     # print cube
 
@@ -25,10 +25,10 @@ class Cube:
         plt.close("all")
 
         mt = np.full((3, 3), 7)
-        top = np.concatenate((mt, self.sides[1], mt, mt), axis=1)
+        top = np.concatenate((mt, self.stickers[1], mt, mt), axis=1)
         mid = np.concatenate(
-            (self.sides[4], self.sides[2], self.sides[5], self.sides[3]), axis=1)
-        bot = np.concatenate((mt, self.sides[0], mt, mt), axis=1)
+            (self.stickers[4], self.stickers[2], self.stickers[5], self.stickers[3]), axis=1)
+        bot = np.concatenate((mt, self.stickers[0], mt, mt), axis=1)
         printRepr = np.concatenate((top, mid, bot), axis=0)
         
         plt.imshow(printRepr, interpolation='nearest', cmap=cmap)
@@ -47,13 +47,13 @@ class Cube:
     # rotate bottom face clockwise
 
     def rotateD(self):
-        self.sides[0] = np.rot90(self.sides[0])
+        self.stickers[0] = np.rot90(self.stickers[0])
 
-        tmp = copy.copy(self.sides[4, 2, :])
-        self.sides[4, 2, :] = copy.copy(self.sides[3, 2, :])
-        self.sides[3, 2, :] = copy.copy(self.sides[5, 2, :])
-        self.sides[5, 2, :] = copy.copy(self.sides[2, 2, :])
-        self.sides[2, 2, :] = tmp
+        tmp = copy.copy(self.stickers[4, 2, :])
+        self.stickers[4, 2, :] = copy.copy(self.stickers[3, 2, :])
+        self.stickers[3, 2, :] = copy.copy(self.stickers[5, 2, :])
+        self.stickers[5, 2, :] = copy.copy(self.stickers[2, 2, :])
+        self.stickers[2, 2, :] = tmp
 
     # rotate bottom face counter-clockwise
 
@@ -63,13 +63,13 @@ class Cube:
 
     # rotate top face clockwise
     def rotateU(self):
-        self.sides[1] = np.rot90(self.sides[1])
+        self.stickers[1] = np.rot90(self.stickers[1])
 
-        tmp = copy.copy(self.sides[4, 0, :])
-        self.sides[4, 0, :] = copy.copy(self.sides[2, 0, :])
-        self.sides[2, 0, :] = copy.copy(self.sides[5, 0, :])
-        self.sides[5, 0, :] = copy.copy(self.sides[3, 0, :])
-        self.sides[3, 0, :] = tmp
+        tmp = copy.copy(self.stickers[4, 0, :])
+        self.stickers[4, 0, :] = copy.copy(self.stickers[2, 0, :])
+        self.stickers[2, 0, :] = copy.copy(self.stickers[5, 0, :])
+        self.stickers[5, 0, :] = copy.copy(self.stickers[3, 0, :])
+        self.stickers[3, 0, :] = tmp
 
     # rotate top face counter-clockwise
 
@@ -79,13 +79,13 @@ class Cube:
 
     # rotate front face clockwise
     def rotateF(self):
-        self.sides[2] = np.rot90(self.sides[2])
+        self.stickers[2] = np.rot90(self.stickers[2])
 
-        tmp = copy.copy(self.sides[4, :, 2])
-        self.sides[4, :, 2] = copy.copy(self.sides[0, 0, :])
-        self.sides[0, 0, :] = copy.copy(self.sides[5, :, 0])
-        self.sides[5, :, 0] = copy.copy(self.sides[1, 2, :])
-        self.sides[1, 2, :] = tmp
+        tmp = copy.copy(self.stickers[4, :, 2])
+        self.stickers[4, :, 2] = copy.copy(self.stickers[0, 0, :])
+        self.stickers[0, 0, :] = copy.copy(self.stickers[5, :, 0])
+        self.stickers[5, :, 0] = copy.copy(self.stickers[1, 2, :])
+        self.stickers[1, 2, :] = tmp
 
     # rotate front face counter-clockwise
     def rotateFprime(self):
@@ -95,13 +95,13 @@ class Cube:
     # rotate back face clockwise
 
     def rotateB(self):
-        self.sides[3] = np.rot90(self.sides[3])
+        self.stickers[3] = np.rot90(self.stickers[3])
 
-        tmp = copy.copy(self.sides[5, :, 2])
-        self.sides[5, :, 2] = copy.copy(self.sides[0, 2, :])
-        self.sides[0, 2, :] = copy.copy(self.sides[4, :, 0])
-        self.sides[4, :, 0] = copy.copy(self.sides[1, 0, :])
-        self.sides[1, 0, :] = tmp
+        tmp = copy.copy(self.stickers[5, :, 2])
+        self.stickers[5, :, 2] = copy.copy(self.stickers[0, 2, :])
+        self.stickers[0, 2, :] = copy.copy(self.stickers[4, :, 0])
+        self.stickers[4, :, 0] = copy.copy(self.stickers[1, 0, :])
+        self.stickers[1, 0, :] = tmp
 
     # rotate back face counter-clockwise
     def rotateBprime(self):
@@ -110,13 +110,13 @@ class Cube:
 
     # rotate left face clockwise
     def rotateL(self):
-        self.sides[4] = np.rot90(self.sides[4])
+        self.stickers[4] = np.rot90(self.stickers[4])
 
-        tmp = copy.copy(self.sides[3, :, 2])
-        self.sides[3, :, 2] = copy.copy(np.flip(self.sides[0, :, 0]))
-        self.sides[0, :, 0] = copy.copy(self.sides[2, :, 0])
-        self.sides[2, :, 0] = copy.copy(self.sides[1, :, 0])
-        self.sides[1, :, 0] = np.flip(tmp)
+        tmp = copy.copy(self.stickers[3, :, 2])
+        self.stickers[3, :, 2] = copy.copy(np.flip(self.stickers[0, :, 0]))
+        self.stickers[0, :, 0] = copy.copy(self.stickers[2, :, 0])
+        self.stickers[2, :, 0] = copy.copy(self.stickers[1, :, 0])
+        self.stickers[1, :, 0] = np.flip(tmp)
 
     # rotate left face counter-clockwise
     def rotateLprime(self):
@@ -126,13 +126,13 @@ class Cube:
     # rotate right face clockwise
 
     def rotateR(self):
-        self.sides[5] = np.rot90(self.sides[5])
+        self.stickers[5] = np.rot90(self.stickers[5])
 
-        tmp = copy.copy(self.sides[2, :, 2])
-        self.sides[2, :, 2] = copy.copy(self.sides[0, :, 2])
-        self.sides[0, :, 2] = copy.copy(np.flip(self.sides[3, :, 0]))
-        self.sides[3, :, 0] = copy.copy(np.flip(self.sides[1, :, 2]))
-        self.sides[1, :, 2] = tmp
+        tmp = copy.copy(self.stickers[2, :, 2])
+        self.stickers[2, :, 2] = copy.copy(self.stickers[0, :, 2])
+        self.stickers[0, :, 2] = copy.copy(np.flip(self.stickers[3, :, 0]))
+        self.stickers[3, :, 0] = copy.copy(np.flip(self.stickers[1, :, 2]))
+        self.stickers[1, :, 2] = tmp
 
     # rotate right face counter-clockwise
     def rotateRprime(self):
@@ -141,21 +141,23 @@ class Cube:
 
 
 cube = Cube()
-cube.rotateR()
-cube.rotateR()
-cube.rotateUprime()
-cube.rotateR()
-cube.rotateUprime()
-cube.rotateR()
-cube.rotateU()
-cube.rotateRprime()
-cube.rotateU()
-cube.rotateR()
-cube.rotateR()
-cube.rotateU()
-cube.rotateDprime()
-cube.rotateR()
-cube.rotateUprime()
-cube.rotateRprime()
-cube.rotateD()
+# cube.rotateF()
+
+# cube.rotateR()
+# cube.rotateR()
+# cube.rotateUprime()
+# cube.rotateR()
+# cube.rotateUprime()
+# cube.rotateR()
+# cube.rotateU()
+# cube.rotateRprime()
+# cube.rotateU()
+# cube.rotateR()
+# cube.rotateR()
+# cube.rotateU()
+# cube.rotateDprime()
+# cube.rotateR()
+# cube.rotateUprime()
+# cube.rotateRprime()
+# cube.rotateD()
 cube.plotCube()
--- src/tests/testCube.py
+++ src/tests/testCube.py
@@ -16,187 +16,187 @@ class TestCube:
 
     def testInit(self):
         for i in range(6):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
 
     def testRotateDSolved(self):
         self.cube1.rotateD()
         for i in range(2):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
         for i in range(2, 4):
             assert(np.array_equal(
-                self.cube1.sides[i, :2, :], np.full((2, 3), i)))
+                self.cube1.stickers[i, :2, :], np.full((2, 3), i)))
             assert(np.array_equal(
-                self.cube1.sides[i, 2, :], np.full((3), i + 2)))
-        assert(np.array_equal(self.cube1.sides[4, :2, :], np.full((2, 3), 4)))
-        assert(np.array_equal(self.cube1.sides[4, 2, :], np.full((3), 3)))
-        assert(np.array_equal(self.cube1.sides[5, :2, :], np.full((2, 3), 5)))
-        assert(np.array_equal(self.cube1.sides[5, 2, :], np.full((3), 2)))
+                self.cube1.stickers[i, 2, :], np.full((3), i + 2)))
+        assert(np.array_equal(self.cube1.stickers[4, :2, :], np.full((2, 3), 4)))
+        assert(np.array_equal(self.cube1.stickers[4, 2, :], np.full((3), 3)))
+        assert(np.array_equal(self.cube1.stickers[5, :2, :], np.full((2, 3), 5)))
+        assert(np.array_equal(self.cube1.stickers[5, 2, :], np.full((3), 2)))
 
     def testRotateDPrimeSolved(self):
         self.cube1.rotateDprime()
         for i in range(2):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
-        assert(np.array_equal(self.cube1.sides[2, :2, :], np.full((2, 3), 2)))
-        assert(np.array_equal(self.cube1.sides[2, 2, :], np.full((3), 5)))
-        assert(np.array_equal(self.cube1.sides[3, :2, :], np.full((2, 3), 3)))
-        assert(np.array_equal(self.cube1.sides[3, 2, :], np.full((3), 4)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
+        assert(np.array_equal(self.cube1.stickers[2, :2, :], np.full((2, 3), 2)))
+        assert(np.array_equal(self.cube1.stickers[2, 2, :], np.full((3), 5)))
+        assert(np.array_equal(self.cube1.stickers[3, :2, :], np.full((2, 3), 3)))
+        assert(np.array_equal(self.cube1.stickers[3, 2, :], np.full((3), 4)))
         for i in range(4, 6):
             assert(np.array_equal(
-                self.cube1.sides[i, :2, :], np.full((2, 3), i)))
+                self.cube1.stickers[i, :2, :], np.full((2, 3), i)))
             assert(np.array_equal(
-                self.cube1.sides[i, 2, :], np.full((3), i - 2)))
+                self.cube1.stickers[i, 2, :], np.full((3), i - 2)))
 
     def testRotateUSolved(self):
         self.cube1.rotateU()
         for i in range(2):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
-        assert(np.array_equal(self.cube1.sides[2, 1:, :], np.full((2, 3), 2)))
-        assert(np.array_equal(self.cube1.sides[2, 0, :], np.full((3), 5)))
-        assert(np.array_equal(self.cube1.sides[3, 1:, :], np.full((2, 3), 3)))
-        assert(np.array_equal(self.cube1.sides[3, 0, :], np.full((3), 4)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
+        assert(np.array_equal(self.cube1.stickers[2, 1:, :], np.full((2, 3), 2)))
+        assert(np.array_equal(self.cube1.stickers[2, 0, :], np.full((3), 5)))
+        assert(np.array_equal(self.cube1.stickers[3, 1:, :], np.full((2, 3), 3)))
+        assert(np.array_equal(self.cube1.stickers[3, 0, :], np.full((3), 4)))
         for i in range(4, 6):
             assert(np.array_equal(
-                self.cube1.sides[i, 1:, :], np.full((2, 3), i)))
+                self.cube1.stickers[i, 1:, :], np.full((2, 3), i)))
             assert(np.array_equal(
-                self.cube1.sides[i, 0, :], np.full((3), i - 2)))
+                self.cube1.stickers[i, 0, :], np.full((3), i - 2)))
 
     def testRotateUPrimeSolved(self):
         self.cube1.rotateUprime()
         for i in range(2):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
         for i in range(2, 4):
             assert(np.array_equal(
-                self.cube1.sides[i, 1:, :], np.full((2, 3), i)))
+                self.cube1.stickers[i, 1:, :], np.full((2, 3), i)))
             assert(np.array_equal(
-                self.cube1.sides[i, 0, :], np.full((3), i + 2)))
-        assert(np.array_equal(self.cube1.sides[4, 1:, :], np.full((2, 3), 4)))
-        assert(np.array_equal(self.cube1.sides[4, 0, :], np.full((3), 3)))
-        assert(np.array_equal(self.cube1.sides[5, 1:, :], np.full((2, 3), 5)))
-        assert(np.array_equal(self.cube1.sides[5, 0, :], np.full((3), 2)))
+                self.cube1.stickers[i, 0, :], np.full((3), i + 2)))
+        assert(np.array_equal(self.cube1.stickers[4, 1:, :], np.full((2, 3), 4)))
+        assert(np.array_equal(self.cube1.stickers[4, 0, :], np.full((3), 3)))
+        assert(np.array_equal(self.cube1.stickers[5, 1:, :], np.full((2, 3), 5)))
+        assert(np.array_equal(self.cube1.stickers[5, 0, :], np.full((3), 2)))
 
     def testRotateFSolved(self):
         self.cube1.rotateF()
-        assert(np.array_equal(self.cube1.sides[0, 1:, :], np.full((2, 3), 0)))
-        assert(np.array_equal(self.cube1.sides[0, 0, :], np.full((3), 5)))
-        assert(np.array_equal(self.cube1.sides[1, :2, :], np.full((2, 3), 1)))
-        assert(np.array_equal(self.cube1.sides[1, 2, :], np.full((3), 4)))
+        assert(np.array_equal(self.cube1.stickers[0, 1:, :], np.full((2, 3), 0)))
+        assert(np.array_equal(self.cube1.stickers[0, 0, :], np.full((3), 5)))
+        assert(np.array_equal(self.cube1.stickers[1, :2, :], np.full((2, 3), 1)))
+        assert(np.array_equal(self.cube1.stickers[1, 2, :], np.full((3), 4)))
         for i in range(2, 4):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
-        assert(np.array_equal(self.cube1.sides[4, :, :2], np.full((3, 2), 4)))
-        assert(np.array_equal(self.cube1.sides[4, :, 2], np.full((3), 0)))
-        assert(np.array_equal(self.cube1.sides[5, :, 1:], np.full((3, 2), 5)))
-        assert(np.array_equal(self.cube1.sides[5, :, 0], np.full((3), 1)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
+        assert(np.array_equal(self.cube1.stickers[4, :, :2], np.full((3, 2), 4)))
+        assert(np.array_equal(self.cube1.stickers[4, :, 2], np.full((3), 0)))
+        assert(np.array_equal(self.cube1.stickers[5, :, 1:], np.full((3, 2), 5)))
+        assert(np.array_equal(self.cube1.stickers[5, :, 0], np.full((3), 1)))
 
     def testRotateFPrimeSolved(self):
         self.cube1.rotateFprime()
-        assert(np.array_equal(self.cube1.sides[0, 1:, :], np.full((2, 3), 0)))
-        assert(np.array_equal(self.cube1.sides[0, 0, :], np.full((3), 4)))
-        assert(np.array_equal(self.cube1.sides[1, :2, :], np.full((2, 3), 1)))
-        assert(np.array_equal(self.cube1.sides[1, 2, :], np.full((3), 5)))
+        assert(np.array_equal(self.cube1.stickers[0, 1:, :], np.full((2, 3), 0)))
+        assert(np.array_equal(self.cube1.stickers[0, 0, :], np.full((3), 4)))
+        assert(np.array_equal(self.cube1.stickers[1, :2, :], np.full((2, 3), 1)))
+        assert(np.array_equal(self.cube1.stickers[1, 2, :], np.full((3), 5)))
         for i in range(2, 4):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
-        assert(np.array_equal(self.cube1.sides[4, :, :2], np.full((3, 2), 4)))
-        assert(np.array_equal(self.cube1.sides[4, :, 2], np.full((3), 1)))
-        assert(np.array_equal(self.cube1.sides[5, :, 1:], np.full((3, 2), 5)))
-        assert(np.array_equal(self.cube1.sides[5, :, 0], np.full((3), 0)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
+        assert(np.array_equal(self.cube1.stickers[4, :, :2], np.full((3, 2), 4)))
+        assert(np.array_equal(self.cube1.stickers[4, :, 2], np.full((3), 1)))
+        assert(np.array_equal(self.cube1.stickers[5, :, 1:], np.full((3, 2), 5)))
+        assert(np.array_equal(self.cube1.stickers[5, :, 0], np.full((3), 0)))
 
     def testRotateBSolved(self):
         self.cube1.rotateB()
-        assert(np.array_equal(self.cube1.sides[0, :2, :], np.full((2, 3), 0)))
-        assert(np.array_equal(self.cube1.sides[0, 2, :], np.full((3), 4)))
-        assert(np.array_equal(self.cube1.sides[1, 1:, :], np.full((2, 3), 1)))
-        assert(np.array_equal(self.cube1.sides[1, 0, :], np.full((3), 5)))
+        assert(np.array_equal(self.cube1.stickers[0, :2, :], np.full((2, 3), 0)))
+        assert(np.array_equal(self.cube1.stickers[0, 2, :], np.full((3), 4)))
+        assert(np.array_equal(self.cube1.stickers[1, 1:, :], np.full((2, 3), 1)))
+        assert(np.array_equal(self.cube1.stickers[1, 0, :], np.full((3), 5)))
         for i in range(2, 4):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
-        assert(np.array_equal(self.cube1.sides[4, :, 1:], np.full((3, 2), 4)))
-        assert(np.array_equal(self.cube1.sides[4, :, 0], np.full((3), 1)))
-        assert(np.array_equal(self.cube1.sides[5, :, :2], np.full((3, 2), 5)))
-        assert(np.array_equal(self.cube1.sides[5, :, 2], np.full((3), 0)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
+        assert(np.array_equal(self.cube1.stickers[4, :, 1:], np.full((3, 2), 4)))
+        assert(np.array_equal(self.cube1.stickers[4, :, 0], np.full((3), 1)))
+        assert(np.array_equal(self.cube1.stickers[5, :, :2], np.full((3, 2), 5)))
+        assert(np.array_equal(self.cube1.stickers[5, :, 2], np.full((3), 0)))
 
     def testRotateBprimeSolved(self):
         self.cube1.rotateBprime()
-        print(self.cube1.sides[0, :2, :])
-        assert(np.array_equal(self.cube1.sides[0, :2, :], np.full((2, 3), 0)))
-        assert(np.array_equal(self.cube1.sides[0, 2, :], np.full((3), 5)))
-        assert(np.array_equal(self.cube1.sides[1, 1:, :], np.full((2, 3), 1)))
-        assert(np.array_equal(self.cube1.sides[1, 0, :], np.full((3), 4)))
+        print(self.cube1.stickers[0, :2, :])
+        assert(np.array_equal(self.cube1.stickers[0, :2, :], np.full((2, 3), 0)))
+        assert(np.array_equal(self.cube1.stickers[0, 2, :], np.full((3), 5)))
+        assert(np.array_equal(self.cube1.stickers[1, 1:, :], np.full((2, 3), 1)))
+        assert(np.array_equal(self.cube1.stickers[1, 0, :], np.full((3), 4)))
         for i in range(2, 4):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
-        assert(np.array_equal(self.cube1.sides[4, :, 1:], np.full((3, 2), 4)))
-        assert(np.array_equal(self.cube1.sides[4, :, 0], np.full((3), 0)))
-        assert(np.array_equal(self.cube1.sides[5, :, :2], np.full((3, 2), 5)))
-        assert(np.array_equal(self.cube1.sides[5, :, 2], np.full((3), 1)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
+        assert(np.array_equal(self.cube1.stickers[4, :, 1:], np.full((3, 2), 4)))
+        assert(np.array_equal(self.cube1.stickers[4, :, 0], np.full((3), 0)))
+        assert(np.array_equal(self.cube1.stickers[5, :, :2], np.full((3, 2), 5)))
+        assert(np.array_equal(self.cube1.stickers[5, :, 2], np.full((3), 1)))
 
     def testRotateLSolved(self):
         self.cube1.rotateL()
         for i in range(0, 2):
             assert(np.array_equal(
-                self.cube1.sides[i, :, 1:], np.full((3, 2), i)))
+                self.cube1.stickers[i, :, 1:], np.full((3, 2), i)))
             assert(np.array_equal(
-                self.cube1.sides[i, :, 0], np.full((3), i + 2)))
-        assert(np.array_equal(self.cube1.sides[2, :, 1:], np.full((3, 2), 2)))
+                self.cube1.stickers[i, :, 0], np.full((3), i + 2)))
+        assert(np.array_equal(self.cube1.stickers[2, :, 1:], np.full((3, 2), 2)))
         # inverted
-        assert(np.array_equal(self.cube1.sides[2, :, 0], np.full((3), 1)))
-        assert(np.array_equal(self.cube1.sides[3, :, :2], np.full((3, 2), 3)))
+        assert(np.array_equal(self.cube1.stickers[2, :, 0], np.full((3), 1)))
+        assert(np.array_equal(self.cube1.stickers[3, :, :2], np.full((3, 2), 3)))
         # inverted
-        assert(np.array_equal(self.cube1.sides[3, :, 2], np.full((3), 0)))
+        assert(np.array_equal(self.cube1.stickers[3, :, 2], np.full((3), 0)))
         for i in range(4, 6):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
 
     def testRotateLPrimeSolved(self):
         self.cube1.rotateLprime()
         assert(np.array_equal(
-            self.cube1.sides[0, :, 1:], np.full((3, 2), 0)))
+            self.cube1.stickers[0, :, 1:], np.full((3, 2), 0)))
         assert(np.array_equal(
-            self.cube1.sides[0, :, 0], np.full((3), 3)))    # inverted
+            self.cube1.stickers[0, :, 0], np.full((3), 3)))    # inverted
         assert(np.array_equal(
-            self.cube1.sides[1, :, 1:], np.full((3, 2), 1)))
+            self.cube1.stickers[1, :, 1:], np.full((3, 2), 1)))
         assert(np.array_equal(
-            self.cube1.sides[1, :, 0], np.full((3), 2)))    # inverted
+            self.cube1.stickers[1, :, 0], np.full((3), 2)))    # inverted
         assert(np.array_equal(
-            self.cube1.sides[2, :, 1:], np.full((3, 2), 2)))
+            self.cube1.stickers[2, :, 1:], np.full((3, 2), 2)))
         assert(np.array_equal(
-            self.cube1.sides[2, :, 0], np.full((3), 0)))    # inverted
+            self.cube1.stickers[2, :, 0], np.full((3), 0)))    # inverted
         assert(np.array_equal(
-            self.cube1.sides[3, :, :2], np.full((3, 2), 3)))
+            self.cube1.stickers[3, :, :2], np.full((3, 2), 3)))
         assert(np.array_equal(
-            self.cube1.sides[3, :, 2], np.full((3), 1)))    # inverted
+            self.cube1.stickers[3, :, 2], np.full((3), 1)))    # inverted
         for i in range(4, 6):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
 
     def testRotateRSolved(self):
         self.cube1.rotateR()
         assert(np.array_equal(
-            self.cube1.sides[0, :, :2], np.full((3, 2), 0)))
+            self.cube1.stickers[0, :, :2], np.full((3, 2), 0)))
         assert(np.array_equal(
-            self.cube1.sides[0, :, 2], np.full((3), 3)))    # inverted
+            self.cube1.stickers[0, :, 2], np.full((3), 3)))    # inverted
         assert(np.array_equal(
-            self.cube1.sides[1, :, :2], np.full((3, 2), 1)))
+            self.cube1.stickers[1, :, :2], np.full((3, 2), 1)))
         assert(np.array_equal(
-            self.cube1.sides[1, :, 2], np.full((3), 2)))    # inverted
+            self.cube1.stickers[1, :, 2], np.full((3), 2)))    # inverted
         assert(np.array_equal(
-            self.cube1.sides[2, :, :2], np.full((3, 2), 2)))
+            self.cube1.stickers[2, :, :2], np.full((3, 2), 2)))
         assert(np.array_equal(
-            self.cube1.sides[2, :, 2], np.full((3), 0)))    # inverted
+            self.cube1.stickers[2, :, 2], np.full((3), 0)))    # inverted
         assert(np.array_equal(
-            self.cube1.sides[3, :, 1:], np.full((3, 2), 3)))
+            self.cube1.stickers[3, :, 1:], np.full((3, 2), 3)))
         assert(np.array_equal(
-            self.cube1.sides[3, :, 0], np.full((3), 1)))    # inverted
+            self.cube1.stickers[3, :, 0], np.full((3), 1)))    # inverted
         for i in range(4, 6):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
 
     def testRotateRPrimeSolved(self):
         self.cube1.rotateRprime()
         for i in range(0, 2):
             assert(np.array_equal(
-                self.cube1.sides[i, :, :2], np.full((3, 2), i)))
+                self.cube1.stickers[i, :, :2], np.full((3, 2), i)))
             assert(np.array_equal(
-                self.cube1.sides[i, :, 2], np.full((3), i + 2)))    # inverted
-        assert(np.array_equal(self.cube1.sides[2, :, :2], np.full((3, 2), 2)))
+                self.cube1.stickers[i, :, 2], np.full((3), i + 2)))    # inverted
+        assert(np.array_equal(self.cube1.stickers[2, :, :2], np.full((3, 2), 2)))
         # inverted
-        assert(np.array_equal(self.cube1.sides[2, :, 2], np.full((3), 1)))
-        assert(np.array_equal(self.cube1.sides[3, :, 1:], np.full((3, 2), 3)))
+        assert(np.array_equal(self.cube1.stickers[2, :, 2], np.full((3), 1)))
+        assert(np.array_equal(self.cube1.stickers[3, :, 1:], np.full((3, 2), 3)))
         # inverted
-        assert(np.array_equal(self.cube1.sides[3, :, 0], np.full((3), 0)))
+        assert(np.array_equal(self.cube1.stickers[3, :, 0], np.full((3), 0)))
         for i in range(4, 6):
-            assert(np.array_equal(self.cube1.sides[i], np.full((3, 3), i)))
+            assert(np.array_equal(self.cube1.stickers[i], np.full((3, 3), i)))
